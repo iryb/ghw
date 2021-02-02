8e7d39b134d35d51184f832e47973efd00e04c06
Irina <iryna.burianovata@gmail.com>
2021-02-02 17:14:29 +0200

    Add image to Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello wwww!</div>
+  return <div>
+    <h1>Hello wwww!</h1>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
 }
