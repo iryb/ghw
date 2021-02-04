d722731d15d62d8677beed6aa00c900d896bf214
Irina <iryna.burianovata@gmail.com>
2021-02-04 16:31:57 +0200

    Tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
 import React from "react"
+import Layout from "../components/layout";
 
 export default function Home() {
-  return <div>
-    <h1>Page header</h1>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>
+  return (
+      <Layout>
+        <h1>Page header</h1>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <p>
+          What do I like to do? Lots of course but definitely enjoy building
+          websites.
+        </p>
+      </Layout>
+  );
 }
